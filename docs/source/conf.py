2d230a7692741b5ee96a4b14f2933b7e0f1f5bc0
ycl <529768926@qq.com>
2019-12-10 20:15:34 +0800

    add autoapi

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -13,6 +13,7 @@
 import os
 import sys
 sys.path.insert(0, os.path.abspath('../../'))
+import sphinx
 
 # -- Project information -----------------------------------------------------
 
@@ -64,7 +65,7 @@ pygments_style = 'sphinx'
 todo_include_todos = True
 
 # auto api configuration
-autoapi_dirs = ['../crowdcount']
+autoapi_dirs = ['../../crowdcount']
 
 # -- Options for HTML output -------------------------------------------------
 
