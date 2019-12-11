9644ea7533a174848131f397a30196afc9b67f26
ycl <529768926@qq.com>
2019-12-11 16:37:47 +0800

    update cong

--- docs/conf.py
+++ docs/conf.py
@@ -12,8 +12,7 @@
 #
 import os
 import sys
-sys.path.insert(0, os.path.abspath('../'))
-import crowdcount
+sys.path.insert(0, os.path.abspath('../crowdcount'))
 
 # -- Project information -----------------------------------------------------
 
