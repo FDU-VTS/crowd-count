90b4d5b33cd163575305c63546c1afd1b36eba1f
ycl <529768926@qq.com>
2019-12-11 17:56:04 +0800

    update docs

--- docs/conf.py
+++ docs/conf.py
@@ -12,6 +12,7 @@
 #
 import os
 import sys
+
 sys.path.insert(0, os.path.abspath('../'))
 
 # -- General configuration ---------------------------------------------------
@@ -19,14 +20,22 @@ sys.path.insert(0, os.path.abspath('../'))
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['autoapi.extension',
+extensions = ['sphinx.ext.autodoc',
+              'sphinx.ext.autosummary',
+              'sphinx.ext.doctest',
               'sphinx.ext.intersphinx',
+              'sphinx.ext.todo',
+              'sphinx.ext.coverage',
+              'sphinx.ext.mathjax',
               'sphinx.ext.napoleon',
+              'sphinx.ext.viewcode',
+              'sphinxcontrib.googleanalytics',
               ]
 
-autoapi_type = 'python'
-autoapi_dirs = ['../crowdcount']
-autoapi_generate_api_docs = False
+napoleon_use_ivar = True
+
+googleanalytics_id = 'UA-90545585-1'
+googleanalytics_enabled = True
 
 intersphinx_mapping = {
     'python': ('https://docs.python.org/', None),
